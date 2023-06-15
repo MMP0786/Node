4ebce0ead6c132024fae3558e8dafa4743762b82
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-06-15 06:44:16 +0530

    some changes

--- Practice/express.js
+++ Practice/express.js
@@ -1,31 +1,37 @@
 const express = require("express")
 const app = express();
-const path = require("path")
+const path = require("path");
+const route = express.Router();
+
+
+
 // const rqFil = require('r')
 const dirPath = path.join(__dirname, '/htmls')
-const newD = path.join(dirPath, "/index.html")
+// const newD = path.join(dirPath, "/index.html")
 
-app.use(express.static(newD))
+app.use(express.static(dirPath))
 
 // const requireFileter = (req, res, next)=>{
 //     // let data =4;
-//     if(express.query.name){
+//     if(req.query.name){
 //         res.send("this inside of require")
-//     }else {
-        
-//         next()
-//     }
+//         }else {
+            
+//             next()
+//         } 
 // }
-// app.use(requireFileter)
+// // app.use(requireFileter)
+// // route.use(requireFileter)
+// app.get("/", (req, res)=>{
+//     res.sendFile(newD);
+// })
+// route.get("/home",  (req, res)=>{
+//     res.send("<h1>This is Home</h1>")
+// })
+// app.get("*", (req, res)=>{
+//     res.send("<h1>This is Wrong Data Type</h1>")
+// })
+// // console.log(newD)
 
-app.get("/", (req, res)=>{
-    res.sendFile(newD);
-})
-app.get("/home", (req, res)=>{
-    res.send("<h1>This is Home</h1>")
-})
-app.get("*", (req, res)=>{
-    res.send("<h1>This is Wrong Data Type</h1>")
-})
-// console.log(newD)
+// app.use("/", route)
 app.listen(4500)
