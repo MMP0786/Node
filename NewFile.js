c95679cb18bb73922131e5919f5c643bb0faf883
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-07-10 07:51:57 +0530

    NewFile

--- NewFile.js
+++ NewFile.js
@@ -1,17 +1,53 @@
-const {MongoClient} = require("mongodb");
-const url = "mongodb://localhost:27017/";
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb://0.0.0.0:27017/';
 const client = new MongoClient(url);
 
-const database = "local";
+// Database Name
+const dbName = 'myProject';
 
-console.log("other")
-async function getData(){
-    console.log("not connected yet")
-     await client.connect();
-    console.log("connected")
-    const db = clientn.db(database);
-    const result = db.collection("startup_log");
-    const responce = await result.find({}).toArray();
-    console.log(responce)
+async function dbConnect() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  return db.collection('items');  
 }
-getData()
+
+module.exports = dbConnect;
+// async function insert(){
+//   const data = await dbConnect()
+//   const acc =await data.insertMany(
+//     [{name:"Musharraf", sname:"Panwar", age:21},
+//     {name:"Akram", sname:"Badri", age:21},
+//     {name:"Usman", sname:"Gani", age:21}]
+//     );
+// }
+// // insert();
+
+// async function update(){
+//   const data = await dbConnect()
+//   const acc =await data.updateOne(
+//    {name:"Usman"},
+//    {$set:{age:20, profasion:"Business"}}
+//     );
+//     if(acc.acknowledged){
+//       console.log("Updated Successfull")
+//     }
+// }
+// // update()
+// async function deletes(){
+//   const data = await dbConnect();
+//   const dele = await data.deleteOne({name:"Musharraf"});
+
+// }
+// deletes();
+
+
+
+
+
+  // .then()
+  // .catch(console.error)
+  // .finally(() => client.close());
+
